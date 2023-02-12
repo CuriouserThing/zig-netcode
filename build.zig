4181fbd9218d90a1eb1093a2c1f99a99f6490c3d
TheMostCuriousThing <adelaidekwalter@gmail.com>
2023-02-12 09:29:29 -0500

    Begin update to new Zig build system

--- build.zig
+++ build.zig
@@ -1,28 +1,43 @@
 const std = @import("std");
 
-pub fn linkTo(exe: *std.build.LibExeObjStep) !void {
-    const sodium = exe.builder.addStaticLibrary("sodium", src() ++ "/src/sodium.zig");
-    sodium.setTarget(exe.target);
-    sodium.setBuildMode(exe.build_mode);
+pub const Package = struct {
+    module: *std.Build.Module,
+    sodium: *std.Build.CompileStep,
 
-    const target_info = try std.zig.system.NativeTargetInfo.detect(exe.target);
-    if (target_info.target.os.tag == .windows) {
-        sodium.linkSystemLibrary("advapi32");
-        exe.linkSystemLibrary("ws2_32");
+    pub fn build(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.Mode) !Package {
+        const module = b.createModule(.{
+            .source_file = .{ .path = src() ++ "/src/main.zig" },
+            .dependencies = &.{},
+        });
+
+        const sodium = b.addStaticLibrary(.{
+            .name = "sodium",
+            .root_source_file = .{ .path = src() ++ "/src/sodium.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        const target_info = try std.zig.system.NativeTargetInfo.detect(target);
+        if (target_info.target.os.tag == .windows) {
+            sodium.linkSystemLibrary("advapi32");
+        }
+
+        return Package{
+            .module = module,
+            .sodium = sodium,
+        };
     }
 
-    exe.linkLibrary(sodium);
-    exe.linkLibC();
-    exe.addCSourceFile(src() ++ "/lib/netcode/netcode.c", &.{"-U__MINGW32__"});
-    exe.addIncludePath(src() ++ "/src");
-}
-
-pub fn package(name: []const u8) std.build.Pkg {
-    return .{
-        .name = name,
-        .source = .{ .path = src() ++ "/src/main.zig" },
-    };
-}
+    pub fn linkTo(package: Package, exe: *std.Build.CompileStep) !void {
+        const target_info = try std.zig.system.NativeTargetInfo.detect(exe.target);
+        if (target_info.target.os.tag == .windows) {
+            exe.linkSystemLibrary("ws2_32");
+        }
+        exe.linkLibrary(package.sodium);
+        exe.linkLibC();
+        exe.addCSourceFile(src() ++ "/lib/netcode/netcode.c", &.{"-U__MINGW32__"});
+        exe.addIncludePath(src() ++ "/src");
+    }
+};
 
 inline fn src() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse ".";
