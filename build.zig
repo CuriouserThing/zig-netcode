b687fe11b07adc00e70cd56c7225a15d977aa144
TheMostCuriousThing <adelaidekwalter@gmail.com>
2023-06-26 04:31:27 -0400

    Clean up build

--- build.zig
+++ build.zig
@@ -1,44 +1,73 @@
 const std = @import("std");
 
+inline fn thisDir() []const u8 {
+    return comptime std.fs.path.dirname(@src().file) orelse ".";
+}
+
 pub const Package = struct {
     module: *std.Build.Module,
     sodium: *std.Build.CompileStep,
 
     pub fn build(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.Mode) !Package {
-        const module = b.createModule(.{
-            .source_file = .{ .path = src() ++ "/src/main.zig" },
-            .dependencies = &.{},
-        });
-
-        const sodium = b.addStaticLibrary(.{
-            .name = "sodium",
-            .root_source_file = .{ .path = src() ++ "/src/sodium.zig" },
-            .target = target,
-            .optimize = optimize,
-        });
-        const target_info = try std.zig.system.NativeTargetInfo.detect(target);
-        if (target_info.target.os.tag == .windows) {
-            sodium.linkSystemLibrary("advapi32");
-        }
-
         return Package{
-            .module = module,
-            .sodium = sodium,
+            .module = b.createModule(.{
+                .source_file = .{ .path = thisDir() ++ "/src/main.zig" },
+                .dependencies = &.{},
+            }),
+            .sodium = try buildSodium(b, target, optimize),
         };
     }
 
     pub fn linkTo(package: Package, exe: *std.Build.CompileStep) !void {
-        const target_info = try std.zig.system.NativeTargetInfo.detect(exe.target);
-        if (target_info.target.os.tag == .windows) {
-            exe.linkSystemLibrary("ws2_32");
-        }
-        exe.linkLibrary(package.sodium);
+        exe.addIncludePath(thisDir() ++ "/src");
         exe.linkLibC();
-        exe.addCSourceFile(src() ++ "/lib/netcode/netcode.c", &.{"-U__MINGW32__"});
-        exe.addIncludePath(src() ++ "/src");
+        exe.linkLibrary(package.sodium);
+
+        const target_info = try std.zig.system.NativeTargetInfo.detect(exe.target);
+        if (target_info.target.os.tag == .windows) {}
+        switch (target_info.target.os.tag) {
+            .windows => {
+                exe.linkSystemLibrary("ws2_32");
+                exe.addCSourceFile(thisDir() ++ "/lib/netcode/netcode.c", &.{"-U__MINGW32__"});
+            },
+
+            // TODO: examine linux support for netcode.io
+            .linux => {
+                exe.addCSourceFile(thisDir() ++ "/lib/netcode/netcode.c", &.{});
+            },
+
+            // TODO: examine macos support for netcode.io
+            .macos => {
+                exe.addCSourceFile(thisDir() ++ "/lib/netcode/netcode.c", &.{});
+            },
+
+            else => unreachable,
+        }
     }
 };
 
-inline fn src() []const u8 {
-    return comptime std.fs.path.dirname(@src().file) orelse ".";
+fn buildSodium(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.Mode) !*std.Build.CompileStep {
+    const sodium = b.addStaticLibrary(.{
+        .name = "sodium",
+        .root_source_file = .{ .path = thisDir() ++ "/src/sodium.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const target_info = try std.zig.system.NativeTargetInfo.detect(target);
+    switch (target_info.target.os.tag) {
+        .windows => {
+            sodium.linkSystemLibrary("advapi32");
+        },
+
+        // TODO: examine linux support for netcode.io
+        .linux => {},
+
+        // TODO: examine macos support for netcode.io
+        .macos => {},
+
+        else => unreachable,
+    }
+
+    return sodium;
 }
